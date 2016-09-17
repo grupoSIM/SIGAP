51307c58e65c77470e0ef982c206749139f98392
Fernando <jfc_2k81@hotmail.com>
2016-09-17 09:00:28 -0300

    Revert "Actuallizado el Dataset"
    This reverts commit 28031c09915e18d7db2cc8d28a41aa0aa619e686.

--- frmRemate.vb
+++ frmRemate.vb
@@ -1,18 +1,7 @@
 ﻿Public Class frmRemate
     Private Sub frmRemate_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'TODO: This line of code loads data into the 'BdSIGAP_DataSet2.tb_Remates' table. You can move, or remove it, as needed.
-        Me.Tb_RematesTableAdapter.Fill(Me.BdSIGAP_DataSet2.tb_Remates)
-        'TODO: This line of code loads data into the 'BdSIGAP_DataSet2.tb_Carreras' table. You can move, or remove it, as needed.
-        Me.Tb_CarrerasTableAdapter.Fill(Me.BdSIGAP_DataSet2.tb_Carreras)
-        'TODO: This line of code loads data into the 'BdSIGAP_DataSet2.tb_Jornadas' table. You can move, or remove it, as needed.
-        Me.Tb_JornadasTableAdapter2.Fill(Me.BdSIGAP_DataSet2.tb_Jornadas)
-
-    End Sub
-
-    Private Sub Tb_JornadasBindingSource2BindingNavigator1SaveItem_Click(sender As Object, e As EventArgs)
-        Me.Validate()
-        Me.Tb_JornadasBindingSource2.EndEdit()
-        Me.TableAdapterManager2.UpdateAll(Me.BdSIGAP_DataSet2)
+        'TODO: This line of code loads data into the 'BdSIGAP_DataSet.tb_DetalleRemates' table. You can move, or remove it, as needed.
+        Me.Tb_DetalleRematesTableAdapter.Fill(Me.BdSIGAP_DataSet.tb_DetalleRemates)
 
     End Sub
 End Class
