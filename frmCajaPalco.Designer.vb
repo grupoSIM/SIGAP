60eb8863c950f905aa42f65cac5b9dbfb5061efa
GM <gustavomontiel@gmail.com>
2016-09-21 20:58:03 -0300

    reporte caja

--- frmCajaPalco.Designer.vb
+++ frmCajaPalco.Designer.vb
@@ -22,27 +22,57 @@ Partial Class frmCajaPalco
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim ReportDataSource1 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource()
         Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
+        Me.bdSIGAP_DataSet = New SIGAP.bdSIGAP_DataSet()
+        Me.VwCajaPalcoBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.VwCajaPalcoTableAdapter = New SIGAP.bdSIGAP_DataSetTableAdapters.VwCajaPalcoTableAdapter()
+        CType(Me.bdSIGAP_DataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.VwCajaPalcoBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'ReportViewer1
         '
-        Me.ReportViewer1.Location = New System.Drawing.Point(-2, 4)
+        ReportDataSource1.Name = "DataSet1"
+        ReportDataSource1.Value = Me.VwCajaPalcoBindingSource
+        Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource1)
+        Me.ReportViewer1.LocalReport.ReportEmbeddedResource = "SIGAP.rpCajaPalco.rdlc"
+        Me.ReportViewer1.Location = New System.Drawing.Point(-2, 0)
         Me.ReportViewer1.Name = "ReportViewer1"
-        Me.ReportViewer1.Size = New System.Drawing.Size(285, 257)
+        Me.ReportViewer1.Size = New System.Drawing.Size(328, 359)
         Me.ReportViewer1.TabIndex = 0
+        '
+        'bdSIGAP_DataSet
+        '
+        Me.bdSIGAP_DataSet.DataSetName = "bdSIGAP_DataSet"
+        Me.bdSIGAP_DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'VwCajaPalcoBindingSource
+        '
+        Me.VwCajaPalcoBindingSource.DataMember = "VwCajaPalco"
+        Me.VwCajaPalcoBindingSource.DataSource = Me.bdSIGAP_DataSet
+        '
+        'VwCajaPalcoTableAdapter
+        '
+        Me.VwCajaPalcoTableAdapter.ClearBeforeFill = True
         '
         'frmCajaPalco
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.ClientSize = New System.Drawing.Size(327, 357)
         Me.Controls.Add(Me.ReportViewer1)
         Me.Name = "frmCajaPalco"
         Me.Text = "frmCajaPalco"
+        CType(Me.bdSIGAP_DataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.VwCajaPalcoBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
+    Friend WithEvents VwCajaPalcoBindingSource As BindingSource
+    Friend WithEvents bdSIGAP_DataSet As bdSIGAP_DataSet
+    Friend WithEvents VwCajaPalcoTableAdapter As bdSIGAP_DataSetTableAdapters.VwCajaPalcoTableAdapter
 End Class
--- frmCajaPalco.vb
+++ frmCajaPalco.vb
@@ -1,6 +1,14 @@
 ﻿Public Class frmCajaPalco
     Private Sub frmCajaPalco_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'bdSIGAP_DataSet.VwCajaPalco' table. You can move, or remove it, as needed.
+        Me.VwCajaPalcoTableAdapter.Fill(Me.bdSIGAP_DataSet.VwCajaPalco)
+        Dim setup = Me.ReportViewer1.GetPageSettings()
+        setup.Margins = New System.Drawing.Printing.Margins(1, 1, 1, 1)
+        Me.ReportViewer1.SetPageSettings(setup)
+        Me.ReportViewer1.RefreshReport()
+    End Sub
 
-        Me.ReportViewer1.RefreshReport
+    Private Sub ReportViewer1_Load(sender As Object, e As EventArgs) Handles ReportViewer1.Load
+
     End Sub
 End Class
