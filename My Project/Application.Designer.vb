c89b44cefe37d008562d9b0bc96e8ea3d8b5c479
GM <gustavomontiel@gmail.com>
2016-09-23 17:35:31 -0300

    Puse FillByIdCarrera en reportes de caja y premios

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.SIGAP.frmPrincipal
+            Me.MainForm = Global.SIGAP.frmCajaPalco
         End Sub
     End Class
 End Namespace
--- frmReportePremios.vb
+++ frmReportePremios.vb
@@ -1,8 +1,8 @@
 ﻿Public Class frmReportePremios
     Private Sub frmReportePremios_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'TODO: This line of code loads data into the 'bdSIGAP_DataSet.VwPremios' table. You can move, or remove it, as needed.
-        Me.VwPremiosTableAdapter.Fill(Me.bdSIGAP_DataSet.VwPremios)
-        'Me.VwPremiosTableAdapter.FillByIdCarrera(bdSIGAP_DataSet.VwPremios, 26)
+        'Me.VwPremiosTableAdapter.Fill(Me.bdSIGAP_DataSet.VwPremios)
+        Me.VwPremiosTableAdapter.FillByIdCarrera(bdSIGAP_DataSet.VwPremios, 26) ' <-- CAMBIAR EL 26 POR EL PARAMETRO
         Dim setup = Me.ReportViewer1.GetPageSettings()
         setup.Margins = New System.Drawing.Printing.Margins(1, 1, 1, 1)
         Me.ReportViewer1.SetPageSettings(setup)
