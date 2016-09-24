cb3a2bd1bd2f2ea6aa755f8586d64fb08cb7eeae
GM <gustavomontiel@gmail.com>
2016-09-24 10:09:47 -0300

    falto este

---
+++ frmReporteDevolucionesSeleccionar.vb
@@ -0,0 +1,15 @@
+﻿Public Class frmReporteDevolucionesSeleccionar
+    Private Sub frmReporteDevolucionesSeleccionar_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'BdSIGAP_DataSet.tb_Carreras' table. You can move, or remove it, as needed.
+        Me.Tb_CarrerasTableAdapter.Fill(Me.BdSIGAP_DataSet.tb_Carreras)
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        frmReporteDevoluciones.Show()
+    End Sub
+End Class
