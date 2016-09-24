0face4b8d1b2cd17de22f2c99a518bbc510bcbd3
GM <gustavomontiel@gmail.com>
2016-09-24 09:32:35 -0300

    repo devo

--- frmReporteDevoluciones.Designer.vb
+++ frmReporteDevoluciones.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class frmReportePremios
+Partial Class frmReporteDevoluciones
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -24,26 +24,13 @@ Partial Class frmReportePremios
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Dim ReportDataSource3 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource()
-        Me.VwPremiosBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-        Me.bdSIGAP_DataSet = New SIGAP.bdSIGAP_DataSet()
         Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
-        Me.VwPremiosTableAdapter = New SIGAP.bdSIGAP_DataSetTableAdapters.VwPremiosTableAdapter()
+        Me.bdSIGAP_DataSet = New SIGAP.bdSIGAP_DataSet()
         Me.VwDevolucionesBindingSource = New System.Windows.Forms.BindingSource(Me.components)
         Me.VwDevolucionesTableAdapter = New SIGAP.bdSIGAP_DataSetTableAdapters.VwDevolucionesTableAdapter()
-        CType(Me.VwPremiosBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.bdSIGAP_DataSet, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.VwDevolucionesBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'VwPremiosBindingSource
-        '
-        Me.VwPremiosBindingSource.DataMember = "VwPremios"
-        Me.VwPremiosBindingSource.DataSource = Me.bdSIGAP_DataSet
-        '
-        'bdSIGAP_DataSet
-        '
-        Me.bdSIGAP_DataSet.DataSetName = "bdSIGAP_DataSet"
-        Me.bdSIGAP_DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
         '
         'ReportViewer1
         '
@@ -51,16 +38,15 @@ Partial Class frmReportePremios
         ReportDataSource3.Value = Me.VwDevolucionesBindingSource
         Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource3)
         Me.ReportViewer1.LocalReport.ReportEmbeddedResource = "SIGAP.rpDevoluciones.rdlc"
-        Me.ReportViewer1.Location = New System.Drawing.Point(9, 7)
-        Me.ReportViewer1.Margin = New System.Windows.Forms.Padding(0)
+        Me.ReportViewer1.Location = New System.Drawing.Point(1, 4)
         Me.ReportViewer1.Name = "ReportViewer1"
-        Me.ReportViewer1.ShowBackButton = False
-        Me.ReportViewer1.Size = New System.Drawing.Size(328, 246)
+        Me.ReportViewer1.Size = New System.Drawing.Size(355, 321)
         Me.ReportViewer1.TabIndex = 0
         '
-        'VwPremiosTableAdapter
+        'bdSIGAP_DataSet
         '
-        Me.VwPremiosTableAdapter.ClearBeforeFill = True
+        Me.bdSIGAP_DataSet.DataSetName = "bdSIGAP_DataSet"
+        Me.bdSIGAP_DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
         '
         'VwDevolucionesBindingSource
         '
@@ -71,15 +57,14 @@ Partial Class frmReportePremios
         '
         Me.VwDevolucionesTableAdapter.ClearBeforeFill = True
         '
-        'frmReportePremios
+        'frmReporteDevoluciones
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(351, 262)
+        Me.ClientSize = New System.Drawing.Size(357, 328)
         Me.Controls.Add(Me.ReportViewer1)
-        Me.Name = "frmReportePremios"
+        Me.Name = "frmReporteDevoluciones"
         Me.Text = "Reporte Devoluciones"
-        CType(Me.VwPremiosBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.bdSIGAP_DataSet, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.VwDevolucionesBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
@@ -87,9 +72,7 @@ Partial Class frmReportePremios
     End Sub
 
     Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
-    Friend WithEvents VwPremiosBindingSource As BindingSource
-    Friend WithEvents bdSIGAP_DataSet As bdSIGAP_DataSet
-    Friend WithEvents VwPremiosTableAdapter As bdSIGAP_DataSetTableAdapters.VwPremiosTableAdapter
     Friend WithEvents VwDevolucionesBindingSource As BindingSource
+    Friend WithEvents bdSIGAP_DataSet As bdSIGAP_DataSet
     Friend WithEvents VwDevolucionesTableAdapter As bdSIGAP_DataSetTableAdapters.VwDevolucionesTableAdapter
 End Class
