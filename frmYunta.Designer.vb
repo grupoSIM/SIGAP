2ddec459ee567bf60920b14b3454ad77cfca969b
cris <cris.hol47@gmail.com>
2016-09-28 22:11:56 -0300

    act

--- frmYunta.Designer.vb
+++ frmYunta.Designer.vb
@@ -24,14 +24,23 @@ Partial Class frmYunta
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Me.cbCarrera = New System.Windows.Forms.ComboBox()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.BdSIGAP_DataSet = New SIGAP.bdSIGAP_DataSet()
         Me.TbCarrerasBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BdSIGAP_DataSet = New SIGAP.bdSIGAP_DataSet()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.Tb_CarrerasTableAdapter = New SIGAP.bdSIGAP_DataSetTableAdapters.tb_CarrerasTableAdapter()
         Me.Tb_CarrerasCaballosTableAdapter = New SIGAP.bdSIGAP_DataSetTableAdapters.tb_CarrerasCaballosTableAdapter()
         Me.Tb_CaballosTableAdapter = New SIGAP.bdSIGAP_DataSetTableAdapters.tb_CaballosTableAdapter()
-        CType(Me.BdSIGAP_DataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TbJornadasBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.Tb_JornadasTableAdapter = New SIGAP.bdSIGAP_DataSetTableAdapters.tb_JornadasTableAdapter()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
         CType(Me.TbCarrerasBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BdSIGAP_DataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TbJornadasBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'cbCarrera
@@ -39,30 +48,30 @@ Partial Class frmYunta
         Me.cbCarrera.DataSource = Me.TbCarrerasBindingSource
         Me.cbCarrera.DisplayMember = "Nombre"
         Me.cbCarrera.FormattingEnabled = True
-        Me.cbCarrera.Location = New System.Drawing.Point(82, 21)
+        Me.cbCarrera.Location = New System.Drawing.Point(101, 46)
         Me.cbCarrera.Name = "cbCarrera"
-        Me.cbCarrera.Size = New System.Drawing.Size(252, 21)
+        Me.cbCarrera.Size = New System.Drawing.Size(327, 21)
         Me.cbCarrera.TabIndex = 0
         Me.cbCarrera.ValueMember = "Id"
         '
-        'Label1
+        'TbCarrerasBindingSource
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(32, 24)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(44, 13)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Carrera:"
+        Me.TbCarrerasBindingSource.DataMember = "tb_Carreras"
+        Me.TbCarrerasBindingSource.DataSource = Me.BdSIGAP_DataSet
         '
         'BdSIGAP_DataSet
         '
         Me.BdSIGAP_DataSet.DataSetName = "bdSIGAP_DataSet"
         Me.BdSIGAP_DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
         '
-        'TbCarrerasBindingSource
+        'Label1
         '
-        Me.TbCarrerasBindingSource.DataMember = "tb_Carreras"
-        Me.TbCarrerasBindingSource.DataSource = Me.BdSIGAP_DataSet
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(47, 49)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Carrera:"
         '
         'Tb_CarrerasTableAdapter
         '
@@ -76,19 +85,81 @@ Partial Class frmYunta
         '
         Me.Tb_CaballosTableAdapter.ClearBeforeFill = True
         '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(47, 22)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Jornada:"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DataSource = Me.TbJornadasBindingSource
+        Me.ComboBox1.DisplayMember = "Descripcion"
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(101, 19)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(327, 21)
+        Me.ComboBox1.TabIndex = 3
+        Me.ComboBox1.ValueMember = "Id"
+        '
+        'TbJornadasBindingSource
+        '
+        Me.TbJornadasBindingSource.DataMember = "tb_Jornadas"
+        Me.TbJornadasBindingSource.DataSource = Me.BdSIGAP_DataSet
+        '
+        'Tb_JornadasTableAdapter
+        '
+        Me.Tb_JornadasTableAdapter.ClearBeforeFill = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.cbCarrera)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.ComboBox1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Location = New System.Drawing.Point(23, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(542, 81)
+        Me.GroupBox1.TabIndex = 5
+        Me.GroupBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(297, 121)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Agregar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(292, 150)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(84, 23)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Crear Carrera"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
         'frmYunta
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(686, 385)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.cbCarrera)
+        Me.ClientSize = New System.Drawing.Size(797, 385)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Button1)
         Me.Name = "frmYunta"
         Me.Text = "Yunta, Tríos y Cuartetos"
-        CType(Me.BdSIGAP_DataSet, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.TbCarrerasBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BdSIGAP_DataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TbJornadasBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
@@ -99,4 +170,11 @@ Partial Class frmYunta
     Friend WithEvents Tb_CarrerasTableAdapter As bdSIGAP_DataSetTableAdapters.tb_CarrerasTableAdapter
     Friend WithEvents Tb_CarrerasCaballosTableAdapter As bdSIGAP_DataSetTableAdapters.tb_CarrerasCaballosTableAdapter
     Friend WithEvents Tb_CaballosTableAdapter As bdSIGAP_DataSetTableAdapters.tb_CaballosTableAdapter
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TbJornadasBindingSource As BindingSource
+    Friend WithEvents Tb_JornadasTableAdapter As bdSIGAP_DataSetTableAdapters.tb_JornadasTableAdapter
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
 End Class
--- frmYunta.vb
+++ frmYunta.vb
@@ -1,98 +1,137 @@
 ﻿Public Class frmYunta
-
-    Public ref1 As Integer
+    Public altoYunta As Integer = 100
 
     Private Sub frmYunta_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'BdSIGAP_DataSet.tb_Jornadas' table. You can move, or remove it, as needed.
+        Me.Tb_JornadasTableAdapter.Fill(Me.BdSIGAP_DataSet.tb_Jornadas)
         'TODO: This line of code loads data into the 'BdSIGAP_DataSet.tb_Carreras' table. You can move, or remove it, as needed.
         Me.Tb_CarrerasTableAdapter.Fill(Me.BdSIGAP_DataSet.tb_Carreras)
         'TODO: This line of code loads data into the 'BdSIGAP_DataSet.tb_Caballos' table. You can move, or remove it, as needed.
         Me.Tb_CaballosTableAdapter.Fill(Me.BdSIGAP_DataSet.tb_Caballos)
-        'CargarCaballos()
-        ref1 = 1
+        CargarCaballos()
+
     End Sub
 
     Private Sub cbCarrera_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbCarrera.SelectedIndexChanged
-
-        LimpiarCaballos(ref1)
-
-        CargarCaballos()
+        If (cbCarrera.SelectedValue) Then
+            CargarCaballos()
+        End If
     End Sub
 
     Private Function CargarCaballos()
 
         Dim dtCarrerasCaballos = Tb_CarrerasCaballosTableAdapter.GetDataByCarrera(cbCarrera.SelectedValue)
         Dim i As Integer = 1
-        Dim cabs As DataRow
+        Dim caballo As DataRow
+        Dim nuevoControl As Control
+        Dim alto As Integer = 100
+        Dim ctrlsEjec As Integer = Controls.Count - 1
+        Dim ctrl As Control
 
-        Dim alto As Integer = 60
+        For n As Integer = ctrlsEjec To 1 Step -1
+            ctrl = Controls(n)
+            If (ctrl.Tag IsNot Nothing) Then
+                Controls.Remove(ctrl)
+                ctrl.Dispose()
+            End If
+        Next
 
         For Each row As DataRow In dtCarrerasCaballos.Rows
-            cabs = BdSIGAP_DataSet.tb_Caballos.FindById(row("IdCaballo"))
+            caballo = BdSIGAP_DataSet.tb_Caballos.FindById(row("IdCaballo"))
             'MsgBox(cabs("CaballoNombre"), MsgBoxStyle.Exclamation + MsgBoxStyle.OkOnly)
 
-            'Dim cabaNom As String
+            'Crea elckeck box de caballo
             Dim ubicacion As Point
+            ubicacion = New Point(35, alto)
 
-            ubicacion = New Point(25, alto)
-            CrearCheck(ubicacion, cabs("CaballoNombre"), i)
+            nuevoControl = New CheckBox
+            With nuevoControl
+                .Name = "ckCaballo" & i
+                .Size() = New Size(200, 20)
+                .Text = caballo("CaballoNombre")
+                .Location = ubicacion
+                .Tag = "Caballo"
+            End With
+            Controls.Add(nuevoControl)
 
-            'MsgBox(caballo("CaballoNombre"), MsgBoxStyle.Exclamation + MsgBoxStyle.OkOnly)
-            i = i + 1
+
+            ' Creación de TextBox de orden de caballos
+            nuevoControl = New TextBox
+            With nuevoControl
+                .Name = "txtNumero" & i
+                .Location = New Point(240, alto)
+                .Size() = New Size(40, 20)
+                .Text = row("Orden")
+                .Tag = "Numero"
+            End With
+            Controls.Add(nuevoControl)
+
+            'MessageBox.Show(nuevocontrol.Name, "SiGAp", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+            i += 1
             alto = alto + 20
         Next
-        'ref1 = cbCarrera.SelectedValue
 
-        'MessageBox.Show(ref1, "SiGAp", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
-        ref1 = cbCarrera.SelectedValue
-
-    End Function
-
-
-    Private Function CrearCheck(ByVal location As Point, ByVal caballo As String, ByVal nro As Integer)
-
-        Dim nuevocontrol As Control
-
-        nuevocontrol = New CheckBox
-        nuevocontrol.Name = "ckCaballo" & nro
-        nuevocontrol.Width = 2000
-        nuevocontrol.Text = caballo
-        nuevocontrol.Location = location
-        nuevocontrol.Visible = True
-
-        Me.Controls.Add(nuevocontrol)
-        MessageBox.Show(nuevocontrol.Name, "SiGAp", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
-
-    End Function
-
-
-    Private Function LimpiarCaballos(ByVal IdCombo As Integer)
-
-        Dim dtCarrerasCaballos = Tb_CarrerasCaballosTableAdapter.GetDataByCarrera(IdCombo)
-        Dim i As Integer = 1
-        Dim cabs As DataRow
-
-        'Dim alto As Integer = 60
-
-        For Each row As DataRow In dtCarrerasCaballos.Rows
-
-            cabs = BdSIGAP_DataSet.tb_Caballos.FindById(row("IdCaballo"))
-            MessageBox.Show(cabs("CaballoNombre"), "SiGAp", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
-            MessageBox.Show("ckCaballo" & i, "SiGAp", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
-            Me.Controls("ckCaballo" & i).Visible = False
-            Me.Controls("ckCaballo" & i).Text = ""
-            i = i + 1
+        For Each chk As CheckBox In Controls.OfType(Of CheckBox)
+            chk.Visible = True
         Next
-        'ref1 = cbCarrera.SelectedValue
 
-        'MessageBox.Show("limpia", "SiGAp", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
-
+        For Each txt As TextBox In Controls.OfType(Of TextBox)
+            txt.Visible = True
+        Next
 
     End Function
 
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim nuevoControl As Control
+        Dim i As Integer = 1
+        Dim nombrecillo As String
+        nombrecillo = " "
+        Dim num As String = ""
+        'Dim alto As Integer = 90
+        Dim ubicacion As Point
+        ubicacion = New Point(35, altoYunta)
 
 
+        For Each chk As CheckBox In Controls.OfType(Of CheckBox)
+            If chk.Checked = True Then
+                nombrecillo = nombrecillo & chk.Text & "/"
+                num = num & Me.Controls("txtNumero" & i).Text
+            End If
+            i += 1
+        Next
+
+        nuevoControl = New TextBox
+        With nuevoControl
+            .Name = "txtNum" & i
+            .Location = New Point(400, altoYunta)
+            .Size() = New Size(40, 20)
+            .Text = num
+            '.Tag = "Numero"
+            .Visible = True
+        End With
+        Controls.Add(nuevoControl)
+
+        nuevoControl = New TextBox
+        With nuevoControl
+            .Name = "txtCaballo" & i
+            .Location = New Point(450, altoYunta)
+            .Size() = New Size(300, 20)
+            .Text = nombrecillo
+            '.Tag = "Numero"
+            .Visible = True
+        End With
+        Controls.Add(nuevoControl)
+
+        altoYunta = altoYunta + 20
 
 
+        For Each chk As CheckBox In Controls.OfType(Of CheckBox)
+            If chk.Checked = True Then
+                chk.Checked = False
+            End If
+            i += 1
+        Next
 
+    End Sub
 End Class
