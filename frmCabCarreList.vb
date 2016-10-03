506b56f7b67e82726454417c2e55a9de3910c0d5
cris <cris.hol47@gmail.com>
2016-10-02 21:11:18 -0300

    act

--- frmCabCarreList.vb
+++ frmCabCarreList.vb
@@ -39,7 +39,7 @@
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Tb_CarrerasCaballosDataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.None
+        Me.Tb_CarrerasCaballosDataGridView.DataSource = Nothing
         Me.Close()
     End Sub
 End Class
