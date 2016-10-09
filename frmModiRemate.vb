e1a46e6c6ae6e6f5dbc709549cbeab514acee507
Fernando <jfc_2k81@hotmail.com>
2016-10-09 14:05:37 -0300

    Arreglos en el modificar remate y en los reportes

--- frmModiRemate.vb
+++ frmModiRemate.vb
@@ -118,7 +118,7 @@
         Dim cont As Integer
         Dim i As Integer = 0
 
-        For DetalleRemate As Integer = 0 To Me.DataGridView.RowCount - 2
+        For DetalleRemate As Integer = 0 To Me.DataGridView.RowCount - 1
             If Me.DataGridView.Rows(i).Cells("Incluido").Value = True Then
                 cont += 1
                 'MessageBox.Show(cont, "SiGAp", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
@@ -139,7 +139,7 @@
         Dim totalApuesta As Integer = 0
         Dim i As Integer = 0
 
-        For DetalleRemate As Integer = 0 To Me.DataGridView.RowCount - 2
+        For DetalleRemate As Integer = 0 To Me.DataGridView.RowCount - 1
             If Me.DataGridView.Rows(i).Cells("Incluido").Value = True Then
                 totalApuesta = totalApuesta + Me.DataGridView.Rows(i).Cells("ImporteApuesta").Value
                 'MessageBox.Show(cont, "SiGAp", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
@@ -152,7 +152,7 @@
     Private Function ModificarPremio(premio As Decimal)
         Dim i As Integer = 0
 
-        For DetalleRemate As Integer = 0 To Me.DataGridView.RowCount - 2
+        For DetalleRemate As Integer = 0 To Me.DataGridView.RowCount - 1
             If Me.DataGridView.Rows(i).Cells("Incluido").Value = True Then
                 Me.DataGridView.Rows(i).Cells("ImportePremio").Value = premio
                 'MessageBox.Show(cont, "SiGAp", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
